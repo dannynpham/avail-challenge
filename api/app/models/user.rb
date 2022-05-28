6d881865e7165190be32c9f0d1f76fdd3e58da2c
Danny Pham <dannynpham11@gmail.com>
2022-05-27 23:13:06 -0700

    Formatted rails models.

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   def as_json(options = {})
-    super(options.merge({ except: [:password_digest, :last_login_at, :created_at, :updated_at] }))
+    super(options.merge({ except: %i[password_digest last_login_at created_at updated_at] }))
   end
-
 end
