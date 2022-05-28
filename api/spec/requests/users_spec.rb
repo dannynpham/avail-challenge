6b22da540992235fd7ca9dd9a6f4de195595e17a
Danny Pham <dannynpham11@gmail.com>
2022-05-28 03:27:45 -0700

    Added remaining unit tests for user controller.

--- api/spec/requests/users_spec.rb
+++ api/spec/requests/users_spec.rb
@@ -21,4 +21,61 @@ RSpec.describe SessionsController do
       end
     end
   end
+
+  describe 'POST /users' do
+    context 'with valid params' do
+      def valid_request
+        post users_url(params: { user: { email: 'valid@email.com', password: 'password', name: 'Valid Name' } })
+      end
+
+      context 'creates a new user' do
+        include_examples 'API returns status and renders', :success, :user do
+          def make_request
+            post users_url(params: { user: { email: 'valid@email.com', password: 'password', name: 'Valid Name' } })
+          end
+        end
+      end
+
+      it 'increases user count' do
+        expect { valid_request }.to(change { User.count })
+      end
+    end
+
+    context 'with invalid params' do
+      def invalid_request
+        post users_url(params: { user: { email: nil, password: nil } })
+      end
+
+      context 'does not create a new user' do
+        include_examples 'API returns status and renders', :unprocessable_entity, :error do
+          def make_request
+            post users_url(params: { user: { email: nil, password: nil } })
+          end
+        end
+      end
+
+      it 'does not increase user count' do
+        expect { invalid_request }.not_to(change { User.count })
+      end
+    end
+  end
+
+  describe 'GET /me' do
+    context 'with valid session returns current user' do
+      include_examples 'API returns status and renders', :success, :user do
+        def make_request
+          sign_in_user
+          get me_url
+        end
+      end
+    end
+
+    context 'without valid session returns error' do
+      include_examples 'API returns status and renders', :unprocessable_entity, :error do
+        def make_request
+          get me_url
+        end
+      end
+    end
+  end
 end
