bbe6689b5484f652a0207f74edcd790cace3c3c5
Danny Pham <dannynpham11@gmail.com>
2022-05-28 03:07:39 -0700

    Added unit tests for get user index.

---
+++ api/spec/requests/users_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController do
+  include RequestHelpers::SignInHelpers
+
+  describe 'GET /users' do
+    context 'with valid session returns list of users' do
+      include_examples 'API returns status and renders', :success, :user_list do
+        def make_request
+          sign_in_user
+          get users_url
+        end
+      end
+    end
+
+    context 'without valid session returns error' do
+      include_examples 'API returns status and renders', :unprocessable_entity, :error do
+        def make_request
+          get users_url
+        end
+      end
+    end
+  end
+end
